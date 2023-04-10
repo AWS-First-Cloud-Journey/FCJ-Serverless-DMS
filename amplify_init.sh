7f1a588a4b4f1fe146cd52260f3923c25878d894
PhamTHHanh <103091745+PhamTHHanh@users.noreply.github.com>
2023-04-10 15:45:13 +0700

    Initial commit

---
+++ amplify_init.sh
@@ -0,0 +1,40 @@
+
+#!/bin/bash
+set -e
+IFS='|'
+
+AUTHCONFIG="{\
+\"userPoolId\": \"ap-southeast-1_ClqyVYv6m\",\
+\"webClientId\": \"4rt2l9rmqe24vb79pupblc30o0\",\
+\"identityPoolId\": \"ap-southeast-1:507b214d-b57c-455b-b627-fe2bddee56f3\"\
+}"
+
+AWSCLOUDFORMATIONCONFIG="{\
+\"configLevel\":\"project\",\
+\"useProfile\":true,\
+\"profileName\":\"default\"\
+}"
+
+AMPLIFY="{\
+\"envName\":\"fcjdms\"\
+}"
+
+PROVIDERS="{\
+\"awscloudformation\":$AWSCLOUDFORMATIONCONFIG\
+}"
+
+STORAGECONFIG="{\
+  \"region\": \"ap-southeast-1\",\
+  \"bucketName\": \"fcjdmsstore\"\
+}"
+
+CATEGORIES="{\
+\"auth\":$AUTHCONFIG,\
+\"storage\":$STORAGECONFIG\
+}"
+
+amplify init \
+--amplify $AMPLIFY \
+--providers $PROVIDERS \
+--categories $CATEGORIES \
+--yes
